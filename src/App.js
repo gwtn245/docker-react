64ab030e5b82a9695755cf8134513bccd64018dd
gwtn245 <genta.tech1@gmil.com>
2019-01-31 16:45:49 +0900

    changed app.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            yes, it's changed
+            Changed at feature branch
           </p>
           <a
             className="App-link"
